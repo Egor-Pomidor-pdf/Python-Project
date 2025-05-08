5b4074e71460f19d0ac8584d24f2848e7f9d8b76
unmakame <bayanovlad@gmail.com>
2025-05-09 01:07:23 +0300

    чтото наделал (прост по прикольчику)

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import './RegisterPage.css';
 
 const RegisterPage = () => {
   const [firstName, setFirstName] = useState("");
@@ -88,7 +89,7 @@ const RegisterPage = () => {
           type="text"
           placeholder="Пароль"
         />
-        <button>Заркгистрироваться</button>
+        <button>Зарeгистрироваться</button>
       </form>
     </div>
   );
