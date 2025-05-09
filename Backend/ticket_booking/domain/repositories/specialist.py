3323a7258538927e3b35b0532187000fdc509a81
Georgy <g.v.strokov@yandex.ru>
2025-05-09 03:30:40 +0300

    the authentication function has been changed, verification has been added

--- Backend/ticket_booking/domain/repositories/specialist.py
+++ Backend/ticket_booking/domain/repositories/specialist.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from ticket_booking.domain.models.specialist import Specialist 
 
 
@@ -10,3 +11,8 @@ class SpecialistRepository:
     async def register_specialist(self, Username: str, Role: str):
         self.session.add(Specialist(username=Username, role=Role))
         self.session.commit()
+
+    async def is_specialist(self, Username: str):
+        result = await self.session.execute(select(Specialist).where(Specialist.username == Username))
+
+        return result.scalars().first() is not None
