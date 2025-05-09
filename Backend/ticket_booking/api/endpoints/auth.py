6be2547b2e8462eb228afcf2b7713d33489e9da8
Georgy <g.v.strokov@yandex.ru>
2025-05-09 03:44:46 +0300

    Small changes

--- Backend/ticket_booking/api/endpoints/auth.py
+++ Backend/ticket_booking/api/endpoints/auth.py
@@ -13,7 +13,8 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
     try:
         user_repo = UserRepository(db)
-        auth_service = AuthService(user_repo)
+        specialist_repo = SpecialistRepository(db)
+        auth_service = AuthService(user_repo, specialist_repo)
         return await auth_service.register_user(user.dict())
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -24,7 +25,8 @@ async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
 async def login(user: UserLogin, db: AsyncSession = Depends(get_db)):
     try:
         user_repo = UserRepository(db)
-        auth_service = AuthService(user_repo)
+        specialist_repo = SpecialistRepository(db)
+        auth_service = AuthService(user_repo, specialist_repo)
         return await auth_service.authenticate_user(user.login, user.password)
     except Exception as e:
         raise HTTPException(status_code=401, detail=str(e))
